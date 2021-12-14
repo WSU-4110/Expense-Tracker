80b9bc6003b5bac850d5e8787c7f23e3c83659cb
Donyell Williams <av1122@wayne.edu>
2021-12-13 23:41:46 -0500

    adding missing work to git

--- sqlConnection.php
+++ sqlConnection.php
@@ -1,76 +1,80 @@
 <?php
 
-class sqlConnection {
+	class sqlConnection {
 	
-	private $hostName; 
-	private $dbName; 
-	private $dbUserName;
-	private $password;
-	public $conn;
-	public $sqlconn;
+		public $hostName; 
+		public $dbName; 
+		private $dbUserName;
+		private $password;
+		public $conn;
+		public $sql; //sql query being completed
+		public $sqlconn; //A bool to test connections
 	
 
-	function __construct(){
-		$this->hostName = "localhost";
-		$this->dbName = "id17843849_expensetrackerdb";
-		$this->dbUserName = "id17843849_expensetrackerdbuser";
-		$this->password = "AVpczEYqz;*?[8/";
-	}
+		function __construct(){
+			$this->hostName = "localhost";
+			$this->dbName = "id17843849_expensetrackerdb";
+			$this->dbUserName = "id17843849_expensetrackerdbuser";
+			$this->password = "AVpczEYqz;*?[8/";
+		}
 
-	function connectToDatabase() {
-		
-		// Create connection
-		$conn = new mysqli($this->hostName, $this->dbUserName, $this->password, $this->dbName);
-		
-	}
+		function connectToDatabase() {
+			
+			$SQLConnection = new sqlConnection();
+			$SQLConnection->conn = new mysqli($SQLConnection->hostName, $SQLConnection->dbUserName, $SQLConnection->password, $SQLConnection->dbName,"3306");
 
-	function checkConnection(){
-		if ($conn->connect_error) {
+			if ($SQLConnection-> conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+			$this->conn = $SQLConnection->conn;
+
+			//return $SQLConnection->conn;
+			return $this->conn;
+		}
+
+		function checkConnection(){
 			$sqlconn = False;
-			die("Connection to $this->dbName failed: " . $conn->connect_error);
+			if ($conn->connect_error) {
+				die("Connection to $this->dbName failed: " . $conn->connect_error);
+			}
+			else{ 
+				$sqlconn = True; 
+			}
 			return $sqlconn;
 		}
-		else{ 
-			$sqlconn = True; 
-			return $sqlconn;
+
+		function closeConnetion(){
+			$conn->close();
+		}
+
+		function setHostName($hostName){
+			$this->hostName = $hostName;
+		}
+
+		function setDBName($dbName){
+			$this->dbName = $dbName;
+		}
+
+		function setdbUserName($dbUserName){
+			$this->dbUserName = $dbUserName;
+		}
+
+		function setPassword($password){
+			$this->password = $password;
+		}
+
+		function getHostName(){
+			return $this->hostName;
+		}
+
+		function getDBName(){
+			return $this->dbName;
+		}
+
+		function getdbUserName(){
+			return $this->dbUserName;
+		}
+
+		function getPassword(){
+			return $this->password;
 		}
 	}
-
-	function closeConnetion(){
-		$conn->close();
-	}
-
-	function setHostName($hostName){
-		$this->hostName = $hostName;
-	}
-
-	function setDBName($dbName){
-		$this->dbName = $dbName;
-	}
-
-	function setdbUserName($dbUserName){
-		$this->dbUserName = $dbUserName;
-	}
-
-	function setPassword($password){
-		$this->password = $password;
-	}
-
-	function getHostName(){
-		return $this->hostName;
-	}
-
-	function getDBName(){
-		return $this->dbName;
-	}
-
-	function getdbUserName(){
-		return $this->dbUserName;
-	}
-
-	function getPassword(){
-		return $this->password;
-	}
-}
-
 ?>
